f2766bd2233aa06866df8e0be58b4a3ca3dabef5
AnthonyRivera3 <Anthony.rivera01110@gmail.com>
2025-04-06 00:30:56 -0400

    Python 100 day challenge Day 1
    Learning String concatenation, variables, input, output

---
+++ 100_days_of_python/day1.py
@@ -0,0 +1,13 @@
+#creating a band name generator program
+
+print("Welcome to the band name generator! ")
+Location = input("What is the name of the city you grew up in?\n")
+animal = input("What's your pets name? ")
+print("\nYour band name could be " + Location + " " + animal )
+
+ 
+    
+    
+
+
+
