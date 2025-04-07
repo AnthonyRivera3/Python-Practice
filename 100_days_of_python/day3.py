1d19ec057b0c054fc6b2b019c29c86c6b4f2d54e
AnthonyRivera3 <Anthony.rivera01110@gmail.com>
2025-04-07 10:18:52 -0400

    day 3 100 days of python
    practice on conditional statements logical operations and code blocks with scope

---
+++ 100_days_of_python/day3.py
@@ -0,0 +1,11 @@
+#conditional statements, logical operations, code blocks and scope
+
+
+#exercise 1
+
+num = int(input("Enter a number and I will check if it is odd or even: #"))
+
+if (num % 2) == 0:
+    print("/nThe number is even")
+else:
+    print("/nThe number is odd")
