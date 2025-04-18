d6e7ccd7710c74409224836c1cabb3b2771541fe
AnthonyRivera3 <Anthony.rivera01110@gmail.com>
2025-04-18 14:01:49 -0400

    fixed code for day 5 python challenge

--- 100_days_of_python/day5.py
+++ 100_days_of_python/day5.py
@@ -31,7 +31,7 @@ import random
 
 Uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 Lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-numbers = ['1','2','3','4','5','6','7','8','9','10']
+numbers = ['1','2','3','4','5','6','7','8','9','0']
 Signs = ['!','#','$''!','@','%','&','*']
 
 password = []
@@ -60,6 +60,7 @@ for key in amount3:
 
 random.shuffle(password)
 realpassword = ""
+
 for key in password:
     realpassword+=key
 
