baf2061d2a6079edb993f9dc6223dabe5549822c
AnthonyRivera3 <Anthony.rivera01110@gmail.com>
2025-04-18 23:19:07 -0400

    day 7 of python coding
    creating hangman game not done yet issue with while loop condition

--- 100_days_of_python/day7.py
+++ 100_days_of_python/day7.py
@@ -1,1 +1,42 @@
 #Creating a Hangman project game
+import random
+
+words = ['panda', 'chicken', 'food', 'drink', 'games', 'fried' , 'bike', 'fruit', 'strawberry', 'monday', 'jello', 'music', 'computer' ]
+
+print("Hello Welcome player!")
+print("We will be playing Hangman!")
+print("Guess the word you get 5 tries:")
+
+
+
+
+
+testcase = 5
+Game_word = random.choice(words)
+
+decoy_word = list(range(len(Game_word)))
+for key in decoy_word:
+    decoy_word[key] = '_'
+
+print(Game_word)
+print(decoy_word)
+
+
+while testcase!=0 and Game_word != decoy_word:
+    guess = input("Guess a letter lowercase only:")
+    if guess in Game_word:
+     position = Game_word.index(guess)
+     decoy_word[position] = guess
+     print(decoy_word)
+    else:
+        testcase-=1
+        print("You have ", testcase, " lives remaining")
+        print(decoy_word)
+
+if testcase == 0:
+   print("You lose sorry!")
+else:
+   print("Congrats you have won!")
+
+
+
