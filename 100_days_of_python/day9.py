73dd8d04931e689ec2c8f3bf9c22248cd479fc3f
AnthonyRivera3 <Anthony.rivera01110@gmail.com>
2025-08-25 22:04:43 -0400

    Adding more code to day 9 py
    Pushing back so I can work on some more code

--- 100_days_of_python/day9.py
+++ 100_days_of_python/day9.py
@@ -18,3 +18,24 @@
 #for thing in First_dictonairy:
    # print(First_dictornairy[thing])
 
+# student_scores = {
+#     'Harry': 88,
+#     'Ron': 78,
+#     'Hermione': 95,
+#     'Draco': 75,
+#     'Neville': 60
+# }
+
+# student_grades = {}
+
+# for key in student_scores:
+#     score  = student_scores[key]
+#     if score <= 70:
+#         student_grades[key] = "Fail"
+#     if  score >= 71 and score <= 80:
+#         student_grades[key] = "Acceptable"
+#     if  score >= 81 and score <= 90:
+#         student_grades[key] = "Exceeds Expectations"
+#     if  score >= 91 and score <= 100:
+#         student_grades[key] = "Outstanding"
+        
