433af38f47b8d5e4a876c66954ccd41431bca969
AnthonyRivera3 <Anthony.rivera01110@gmail.com>
2025-08-25 22:02:34 -0400

    day9.py
    Just sending to add more not done yet

---
+++ 100_days_of_python/day9.py
@@ -0,0 +1,20 @@
+#Dictonairy
+#First_dictonairy ={
+# key: value,
+#"Function": "a loop"
+#}
+
+#To call a dictoanry you do this
+#print(First_dictonairy["Function"])
+#output would be: "a loop"
+
+#empty_dictonairy = {}
+
+#wiping dictonairy if its full to just use this
+#empty_dictonairy = {}
+
+
+#This is how you get the values of the dictoanairy. If i wanted just the key then it would be print(thing)
+#for thing in First_dictonairy:
+   # print(First_dictornairy[thing])
+
