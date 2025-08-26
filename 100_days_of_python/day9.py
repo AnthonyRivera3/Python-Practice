52e2ad9aeff36bf8c07b91330615764d9eeb731f
AnthonyRivera3 <Anthony.rivera01110@gmail.com>
2025-08-25 23:45:00 -0400

    Final update of the code day9.py
    This is the finished project for todays task. The silent auction

--- 100_days_of_python/day9.py
+++ 100_days_of_python/day9.py
@@ -38,4 +38,55 @@
 #         student_grades[key] = "Exceeds Expectations"
 #     if  score >= 91 and score <= 100:
 #         student_grades[key] = "Outstanding"
-        
+      
+
+
+#-------------------------------------------------------------------------
+#Nested lists and dictonairies
+
+# travel_log = {
+#   "france" : ["paris", "Lille", "Djon"]  
+
+# }
+# print(travel_log["france"][1])
+
+
+# nested_list = ["a", "b", ["c", "d"]]
+# #2 is to go into the second brackets and 1 is the element inside those brackets
+# print(nested_list[2][1])
+
+# travel_log = {
+    
+# "France": {
+#     "num_cities_visted": 8,
+#     "num_times visited": 3,
+#     "cities": ["paris", "lille", "Dijon"]
+# }
+# }
+
+# print(travel_log["France"]["cities"][2])
+
+#----------------------------------------------------------------------------------------------------
+bidders = {}
+
+def addbidder():
+    name = input("What is your name?: \n")
+    bid = int(input("What is your bid?: \n"))
+    bidders[name] = bid
+    answer = input("Is there another bidder? Enter: 'y' or 'n' \n")
+    if answer == 'y':
+        print("\n" * 100) 
+        addbidder()
+    else:
+        top_bidder = 0
+        winner = ""
+        for key in bidders:
+            money = bidders[key]
+            if money > top_bidder:
+                top_bidder = money
+                winner = key
+        print(f"The winner is {winner} with a bid of ${top_bidder}")
+
+addbidder()
+                 
+             
