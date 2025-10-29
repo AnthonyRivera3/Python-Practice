2ee4859cb6f7780a85759d41e98ebada024b09a3
AnthonyRivera3 <Anthony.rivera01110@gmail.com>
2025-10-29 08:19:49 -0400

    Day 16 python
    making objects and calling attributes inside of a class to get a better understanding of Object oriented programming in python

--- 100_days_of_python/IntermediateLevel/day16/day16coffeeproject_final/main.py
+++ 100_days_of_python/IntermediateLevel/day16/day16coffeeproject_final/main.py
@@ -1,4 +1,32 @@
 from menu import Menu
+from menu import MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+
+#Objects
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+
+
+var = True
+
+
+
+while var:
+    options = menu.get_items()
+    choice = input(f"What would you like ({options}: )")
+    if choice == 'off':
+        var == False
+    elif choice == "report":
+        print(money_machine.report())
+        print(coffee_maker.report())
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink): 
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
 
